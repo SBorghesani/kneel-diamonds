c2acbcc2954aa9b0e3354f21e1fbfdd00aeb49c9
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-07-30 12:03:07 -0500

    The jewelry options module now has a working radio button after page regen

--- scripts/jewelry-options.js
+++ scripts/jewelry-options.js
@@ -1,6 +1,7 @@
-import { getJewelryOptions, setJewelryOption } from "./database.js"
+import { getJewelryOptions, setJewelryOption, getOrderBuilder, getMetals } from "./database.js"
 
 const jewelryOptions = getJewelryOptions()
+const orderBuilder = getOrderBuilder()
 
 document.addEventListener(
     "change",
@@ -19,13 +20,22 @@ export const JewelryOptions = () => {
     let html = "<ul class='jewelryOptions'>"
 
     const listedItems = jewelryOptions.map(jewelryOption => {
-        return `<li calss="jewelryOption">
-            <input type="radio" name="jewelryOption" value="${jewelryOption.id}" /> ${jewelryOption.option}
-        </li>`
+        if (jewelryOption.id === orderBuilder.jewelryOptionId) {
+            return `<li>
+            <input type="radio" name="jewelryOption" value="${jewelryOption.id}" checked/> ${jewelryOption.option}
+            </li>`
+        } else {
+            return `<li>
+            <input type="radio" name="jewelryOption" value="${jewelryOption.id}"/> ${jewelryOption.option}
+            </li>`
+        } 
     })
-
+    
     html += listedItems.join("")
     html += "</ul>"
-
+    
     return html
 }
+// return `<li calss="jewelryOption">
+//     <input type="radio" name="jewelryOption" value="${jewelryOption.id}" /> ${jewelryOption.option}
+// </li>`
